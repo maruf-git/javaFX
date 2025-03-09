339f5d6947fc4ede302206e21a7ec38ec33a16cc
orion <munna.csehstu@gmail.com>
2025-03-09 13:56:37 +0600

    stage,scene,icon,title,widow size & resize

--- JavaFx2/src/main/java/com/example/javafx2/HelloApplication.java
+++ JavaFx2/src/main/java/com/example/javafx2/HelloApplication.java
@@ -2,22 +2,54 @@ package com.example.javafx2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application{
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+
+        Scene scene = new Scene(root, Color.BLUE);
+        stage.setTitle("Barta - Desktop Application");
+//        Image icon = new Image("/src/icon.png");
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/icon.png")).toExternalForm());
+        stage.getIcons().add(icon);
+
+        stage.setX(100);
+        stage.setY(500);
+        stage.setWidth(500);
+        stage.setHeight(300);
+
+        stage.setResizable(false);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Press q to exit");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
         stage.setScene(scene);
         stage.show();
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
 }
+
+//public class HelloApplication extends Application {
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch();
+//    }
+//}
