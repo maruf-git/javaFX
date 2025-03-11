04ff0ce10ae259f330a4628b1ed09d36bc3c0b67
orion <munna.csehstu@gmail.com>
2025-03-11 20:15:52 +0600

    adding css to JavaFX

--- JavaFX5/src/main/java/com/example/javafx5/HelloApplication.java
+++ JavaFX5/src/main/java/com/example/javafx5/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
         String css = Objects.requireNonNull(this.getClass().getResource("application.css")).toExternalForm();
         scene.getStylesheets().add(css);
 
+
         stage.setScene(scene);
         stage.show();
     }
